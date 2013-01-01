e1701a4874460caffc364e7258068e35f433a515
Cedric Porter <cedricporter@gmail.com>
2013-01-01 23:35:04 +0800

    oh...

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+ln -s ~/vim-emacs-setting/emacs/.emacs ~/.emacs
+ln -s ~/vim-emacs-setting/emacs/.emacs.d ~/.emacs.d
+ln -s ~/vim-emacs-setting/vim/.vimrc ~/.vimrc
+ln -s ~/vim-emacs-setting/vim/.vim ~/.vimc
+
+
+
