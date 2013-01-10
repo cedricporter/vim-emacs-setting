a6e182e6ebde0ac186b3a88302532dbd2d76ed6c
Cedric Porter <cedricporter@gmail.com>
2013-01-11 01:17:06 +0800

    fix install.sh bug of link path

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # link to $HOME
 echo "# Creating link in your $HOME dir..." 
-ln -vsf $PWD/vim-emacs-setting/emacs/.emacs ~/.emacs
-ln -vsf $PWD/vim-emacs-setting/emacs/.emacs.d ~/.emacs.d
-ln -vsf $PWD/vim-emacs-setting/vim/.vimrc ~/.vimrc
-ln -vsf $PWD/vim-emacs-setting/vim/.vim ~/.vim
+ln -vsf $PWD/emacs/.emacs ~/.emacs
+ln -vsf $PWD/emacs/.emacs.d ~/.emacs.d
+ln -vsf $PWD/vim/.vimrc ~/.vimrc
+ln -vsf $PWD/vim/.vim ~/.vim
 
 # pull submodule
 echo "# Pulling submodule"
