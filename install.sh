3f8cda342566184425e2342caa26066c3d7cd3ae
Cedric Porter <cedricporter@gmail.com>
2013-01-11 00:54:23 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
 #! /bin/sh
 
-ln -s ~/vim-emacs-setting/emacs/.emacs ~/.emacs
-ln -s ~/vim-emacs-setting/emacs/.emacs.d ~/.emacs.d
-ln -s ~/vim-emacs-setting/vim/.vimrc ~/.vimrc
-ln -s ~/vim-emacs-setting/vim/.vim ~/.vimc
+# link to $HOME
+ln -s $PWD/vim-emacs-setting/emacs/.emacs ~/.emacs
+ln -s $PWD/vim-emacs-setting/emacs/.emacs.d ~/.emacs.d
+ln -s $PWD/vim-emacs-setting/vim/.vimrc ~/.vimrc
+ln -s $PWD/vim-emacs-setting/vim/.vim ~/.vimc
+
+# pull submodule 
+git submodule init
+git submodule update
+
+# auto-complete submdule
+current_dir=$PWD
+cd emacs/.emacs.d/plugins/auto-complete
+echo "cd $PWD"
+git submodule init
+git submodule update
+
+cd $current_dir
+echo "cd $PWD"
+
+
 
 
 
