ba529b4b8e18242ce41b91eac36cf8ae9ac37bae
Cedric Porter <cedricporter@gmail.com>
2013-01-11 01:04:07 +0800

    install for tips

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #! /bin/sh
 
 # link to $HOME
+echo "# Creating link in your $HOME dir..." 
 ln -s $PWD/vim-emacs-setting/emacs/.emacs ~/.emacs
 ln -s $PWD/vim-emacs-setting/emacs/.emacs.d ~/.emacs.d
 ln -s $PWD/vim-emacs-setting/vim/.vimrc ~/.vimrc
 ln -s $PWD/vim-emacs-setting/vim/.vim ~/.vimc
 
-# pull submodule 
+# pull submodule
+echo "# Pulling submodule"
 git submodule init
 git submodule update
 
 # auto-complete submdule
+echo "# Pulling submodule of auto-complete"
 current_dir=$PWD
 cd emacs/.emacs.d/plugins/auto-complete
 echo "cd $PWD"
