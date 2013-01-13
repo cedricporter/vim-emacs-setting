521f6128a6069717b237558e975846934613f5a8
Cedric Porter <cedricporter@gmail.com>
2013-01-13 14:45:19 +0800

    jedi

--- emacs/.emacs.d/plugins/configs/my-python-settings.el
+++ emacs/.emacs.d/plugins/configs/my-python-settings.el
@@ -1,38 +1,66 @@
 ;; author: Hua Liang [Stupid ET]
-;; Time-stamp: <2012-12-31 16:37:40 by Hua Liang>
+;; Time-stamp: <2013-01-13 14:41:23 Sunday by Hua Liang>
 
 
-;;==================== python ====================
-(require 'python)
+;; ==================== jedi ====================
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(add-to-list 'load-path "~/.emacs.d/plugins/pymacs")
+;; deferred
+(add-to-list 'load-path "~/.emacs.d/plugins/deferred")
+(require 'deferred)
+(require 'concurrent)
 
-(require 'pymacs)
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-(autoload 'pymacs-autoload "pymacs")
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+;; ctable
+(add-to-list 'load-path "~/.emacs.d/plugins/ctable")
+(require 'ctable)
 
-;;; Initialize Rope
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t) ;; Too slow when I am saving
+;; epc
+(add-to-list 'load-path "~/.emacs.d/plugins/epc")
+(require 'epc)
 
-(ac-ropemacs-initialize)
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (add-to-list 'ac-sources 'ac-source-ropemacs)))
+;; jedi
+(add-to-list 'load-path "~/.emacs.d/plugins/jedi")
 
-;; ;;load pydb
-;; (require 'pydb)
-;; (autoload 'pydb "pydb" "Python Debugger mode via GUD and pydb" t)
-;;-------------------- python --------------------
+(setq jedi:setup-keys t)
+(autoload 'jedi:setup "jedi" nil t)
+
+(add-hook 'python-mode-hook 'jedi:setup)
+
+(define-key python-mode-map (kbd "C-c r") 'helm-jedi-related-names)
+;; -------------------- jedi --------------------
+
+
+
+;; ;;==================== python ====================
+;; (require 'python)
+
+;; (autoload 'python-mode "python-mode" "Python Mode." t)
+;; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+;; (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+;; (add-to-list 'load-path "~/.emacs.d/plugins/pymacs")
+
+;; (require 'pymacs)
+;; (autoload 'pymacs-apply "pymacs")
+;; (autoload 'pymacs-call "pymacs")
+;; (autoload 'pymacs-eval "pymacs" nil t)
+;; (autoload 'pymacs-exec "pymacs" nil t)
+;; (autoload 'pymacs-load "pymacs" nil t)
+;; (autoload 'pymacs-autoload "pymacs")
+;; ;;(eval-after-load "pymacs"
+;; ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+
+;; ;;; Initialize Rope
+;; (pymacs-load "ropemacs" "rope-")
+;; (setq ropemacs-enable-autoimport t) ;; Too slow when I am saving
+
+;; (ac-ropemacs-initialize)
+;; (add-hook 'python-mode-hook
+;; 	  (lambda ()
+;; 	    (add-to-list 'ac-sources 'ac-source-ropemacs)))
+
+;; ;; ;;load pydb
+;; ;; (require 'pydb)
+;; ;; (autoload 'pydb "pydb" "Python Debugger mode via GUD and pydb" t)
+;; ;;-------------------- python --------------------
 
 
 (provide 'my-python-settings)
