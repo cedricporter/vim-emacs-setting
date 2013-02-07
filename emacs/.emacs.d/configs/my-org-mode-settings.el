7d3ef5a335d2bf06afad8ea666571412ecc4f1a2
Cedric Porter <cedricporter@gmail.com>
2013-02-07 13:40:58 +0800

    org wiki

--- emacs/.emacs.d/configs/my-org-mode-settings.el
+++ emacs/.emacs.d/configs/my-org-mode-settings.el
@@ -1,5 +1,5 @@
 ;; author: Hua Liang [Stupid ET]
-;; Time-stamp: <2013-01-18 01:41:30 Friday by Hua Liang>
+;; Time-stamp: <2013-02-07 13:40:43 Thursday by Hua Liang>
 
 
 (require 'org)
@@ -19,6 +19,40 @@
             (setq yas/trigger-key [tab])
             (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
             (define-key yas/keymap [tab] 'yas/next-field)))
+
+
+;; ==================== css ====================
+(setq org-export-html-style-include-scripts nil
+      org-export-html-style-include-default nil)
+
+(setq org-export-html-style
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/org-style.css\" />")
+;; -------------------- css --------------------
+
+
+;; ==================== wiki ====================
+(require 'org-publish)
+(setq org-publish-project-alist
+      '(
+	("org-wiki-notes"
+	 :base-directory "~/org-wiki/"
+	 :base-extension "org"
+	 :publishing-directory "~/wiki_public_html/"
+	 :recursive t
+	 :publishing-function org-publish-org-to-html
+	 :headline-levels 4             ; Just the default for this project.
+	 :auto-preamble t
+	 )
+	("org-wiki-static"
+	 :base-directory "~/org-wiki/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :publishing-directory "~/wiki_public_html/"
+	 :recursive t
+	 :publishing-function org-publish-attachment
+	 )
+	("wiki" :components ("org-wiki-notes" "org-wiki-static"))
+	))
+;; -------------------- wiki --------------------
 
 ;(load "~/.emacs.d/plugins/org-mode-markdown/markdown.el")
 
