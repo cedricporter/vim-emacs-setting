0de6d9d62e12e4cd23dee78a4e9eef4d831e94ba
Cedric Porter <cedricporter@gmail.com>
2013-07-24 20:45:58 +0800

    apt-get list

---
+++ apt-get-list.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# File: apt-get-list.sh
+#
+# Created: 星期三, 七月 24 2013 by Hua Liang[Stupid ET] <et@everet.org>
+#
+
+apt-get -y install ttf-mscorefonts-installer ack-grep zsh emacs vim dosbox virtualbox
+apt-get build-dep emacs
--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
 # link to $HOME
-echo "# Creating link in your $HOME dir..." 
+echo "# Creating link in your $HOME dir..."
+mv ~/.emacs ~/.emacs-old
+mv ~/.emacs.d ~/.emacs.d-old
+mv ~/.vimrc ~/.vimrc-old
+mv ~/.vim ~/.vim-old
+
 ln -vsf $PWD/emacs/.emacs ~/.emacs
 ln -vsf $PWD/emacs/.emacs.d ~/.emacs.d
 ln -vsf $PWD/vim/.vimrc ~/.vimrc
@@ -12,16 +17,15 @@ echo "# Pulling submodule"
 git submodule init
 git submodule update
 
-# auto-complete submdule
-echo "# Pulling submodule of auto-complete"
-current_dir=$PWD
-cd emacs/.emacs.d/plugins/auto-complete
-echo "cd $PWD"
-git submodule init
-git submodule update
-
-cd $current_dir
-echo "cd $PWD"
+# # auto-complete submdule
+# echo "# Pulling submodule of auto-complete"
+# current_dir=$PWD
+# cd emacs/.emacs.d/plugins/auto-complete
+# echo "cd $PWD"
+# git submodule init
+# git submodule update
+# cd $current_dir
+# echo "cd $PWD"
 
 
 
