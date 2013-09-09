08fddcdff0e5470ef08909534a695dc80e7a5028
Cedric Porter <cedricporter@gmail.com>
2013-09-09 10:22:23 +0800

    apt-get list

--- apt-get-list.sh
+++ apt-get-list.sh
@@ -8,14 +8,16 @@
 apt-get update
 
 apt-get -y install ttf-mscorefonts-installer ack-grep zsh emacs vim dosbox virtualbox inotify-tools
-apt-get -y install libnotify4 python-gobject mercurial git git-svn bzr python-pygments
+apt-get -y install libnotify4 python-gobject mercurial git git-svn bzr python-pygments xclip
 
-apt-get -y graphviz ruby1.9.3 rubygems
+apt-get -y intall graphviz ruby1.9.3 rubygems tmux htop
 
-apt-get build-dep emacs
+apt-get build-dep emacs24
+
+apt-get -y install libnotify-dev python-gobject libnotify4
 
 # for emacs jedi
-apt-get -y install python-pip python-dev build-essential ipython
+apt-get -y install python-pip python-dev build-essential ipython python-openssl
 pip install jedi epc remote-webkit-debug pylint virtualenv
 
 gem install teamocil bundler
