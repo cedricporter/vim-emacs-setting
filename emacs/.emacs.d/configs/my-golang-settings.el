fb3cb4cdf9d299da29f30d5f1f8bf6d4e0d04181
Cedric Porter <cedricporter@gmail.com>
2013-11-05 19:56:16 +0800

    <f5> to run go program

--- emacs/.emacs.d/configs/my-golang-settings.el
+++ emacs/.emacs.d/configs/my-golang-settings.el
@@ -7,7 +7,13 @@
 (defun my-go-mode-hook () 
   (add-hook 'before-save-hook 'gofmt-before-save) 
   (setq tab-width 4
-        indent-tabs-mode 1)) 
+        indent-tabs-mode 1)
+  (local-set-key (kbd "<f5>") 'my-go-run)
+  ) 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(defun my-go-run ()
+  (interactive)
+  (compile (concat "go run " (buffer-file-name))))
 
 ;;; my-golang-settings.el ends here
