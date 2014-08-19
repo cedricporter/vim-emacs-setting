3fb12f5dae2b2b12951a9a2ff352ca4c4e8e7e79
Cedric Porter <cedricporter@gmail.com>
2014-08-19 20:20:06 +0800

    remove useless comment

--- emacs/.emacs.d/configs/my-el-get-settings.el
+++ emacs/.emacs.d/configs/my-el-get-settings.el
@@ -1,15 +1,7 @@
 ;; author: Hua Liang [Stupid ET]
-;; Time-stamp: <2014-08-19 10:09:28 Tuesday by Hua Liang>
+;; Time-stamp: <2014-08-19 20:19:05 Tuesday by Hua Liang>
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-;; (unless (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-;;     (let (el-get-master-branch)
-;;       (goto-char (point-max))
-;;       (eval-print-last-sexp))))
 
 (unless (require 'el-get nil t)
   (url-retrieve
