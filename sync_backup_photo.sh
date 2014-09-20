e142317bbce222b00ee353bf600c13468df1fa25
Cedric Porter <cedricporter@gmail.com>
2014-09-20 14:59:58 +0800

    sync xmp file when sync photos

--- sync_backup_photo.sh
+++ sync_backup_photo.sh
@@ -7,13 +7,16 @@
 
 echo "================================="
 echo "Syncing folder structure\n"
-echo "New start to backup 收藏"
+
+echo "================================="
+echo "New start to backup 收藏\n"
 rsync --size-only --ignore-existing -rvhH --progress --delete /Volumes/Photos2/收藏 /Volumes/Photos/
 
-echo "New start to backup 照片"
+echo "================================="
+echo "New start to backup 照片\n"
 rsync --size-only --ignore-existing -rvhH --progress --delete /Volumes/Photos2/照片 /Volumes/Photos/
 
-# TODO update .xmp files
-# echo "Now updating xmp files"
-# rsync -rv --include '*/' --include '*.xmp' --exclude '*' --prune-empty-dirs /Volumes/Photos2/收藏 /Volumes/Photos/
-# rsync -rv --include '*/' --include '*.xmp' --exclude '*' --prune-empty-dirs /Volumes/Photos2/照片 /Volumes/Photos/
+echo "================================="
+echo "Now updating xmp file\n"
+rsync -rv --include '*/' --include '*.xmp' --exclude '*' --prune-empty-dirs /Volumes/Photos2/收藏 /Volumes/Photos/
+rsync -rv --include '*/' --include '*.xmp' --exclude '*' --prune-empty-dirs /Volumes/Photos2/照片 /Volumes/Photos/
