17366f3d2a8978214f5045e2d91cdce536d66986
Cedric Porter <cedricporter@gmail.com>
2014-09-20 14:45:46 +0800

    echo folder name when backup

--- sync_backup_photo.sh
+++ sync_backup_photo.sh
@@ -5,5 +5,8 @@
 # Created: Saturday, September 20 2014 by Hua Liang[Stupid ET] <et@everet.org>
 #
 
+echo "New start to backup 收藏"
 rsync --size-only --ignore-existing -rvhH --progress --delete /Volumes/Photos2/收藏 /Volumes/Photos/
+
+echo "New start to backup 照片"
 rsync --size-only --ignore-existing -rvhH --progress --delete /Volumes/Photos2/照片 /Volumes/Photos/
